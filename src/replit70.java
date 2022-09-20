86368caf1c602c96a74edbcd63e5f45b86c082be
Abdul Hashimi <awm.hashimi@gmail.com>
2022-09-19 23:02:06 -0400

    70 Completed

---
+++ src/replit70.java
@@ -0,0 +1,12 @@
+public class replit70
+{
+    public static void main(String[] args)
+    {
+        int[] numArray = {45, 78, 12,  67, 55};
+
+        for (int i = 0; i < numArray.length; i++)
+        {
+            System.out.print(numArray[i] + " ");
+        }
+    }
+}
