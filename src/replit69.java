51e4aa33f9794c10aae93c6cd000dd9686dd4d72
Abdul Hashimi <awm.hashimi@gmail.com>
2022-09-19 22:59:43 -0400

    69 Completed

--- src/replit69.java
+++ src/replit69.java
@@ -2,16 +2,18 @@ public class replit69
 {
     public static void main(String[] args)
     {
-        int numIterations = 8;
+        int numIterations = 7;
         int a;
 
-        for(a = 0; a < numIterations; a++) // outer loop for rows
+        for(; 0 < numIterations; numIterations--) // outer loop for rows
         {
             int num=1;
-            for(int b = 0; b <= a; b++) // inner loop for rows
+            for(int b = 0; b < numIterations; b++) // inner loop for rows
             {
                 // printing num with a space
+                if (numIterations >= 1)
                 System.out.print(num+ " ");
+                else break;
 
                 //incrementing value of num
                 num++;
@@ -19,6 +21,27 @@ public class replit69
 
             // ending line after each row
             System.out.println();
+        }
+
+        numIterations = 7;
+
+        for(a = 0; a < numIterations; a++) // outer loop for rows
+        {
+            int num=1;
+            if (a > 0)
+            {
+                for (int b = 0; b <= a; b++) // inner loop for rows
+                {
+                    // printing num with a space
+                    System.out.print(num + " ");
+
+                    //incrementing value of num
+                    num++;
+                }
+
+                // ending line after each row
+                System.out.println();
+            }
         }
     }
 }
