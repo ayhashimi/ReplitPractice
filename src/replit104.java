e8b3c48304aff83aab6d8fbb93c655b87a6edb0c
Abdul Hashimi <awm.hashimi@gmail.com>
2022-09-28 12:40:26 -0400

    104

--- src/replit104.java
+++ src/replit104.java
@@ -1,2 +1,27 @@
-public class replit104 {
+import java.util.Scanner;
+
+public class replit104
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        String [] names = new String[5];
+        int i;
+
+        for (i = 0; i < 5; i++)
+        {
+            names[i] = input.nextLine();
+        }
+
+
+        for (i = 0; i < names.length; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                System.out.print(names[i].charAt(j));
+            }
+            System.out.println();
+        }
+    }
 }
