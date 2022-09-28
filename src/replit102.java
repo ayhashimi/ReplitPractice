560fffb3e0eec90a04764c01b75b69d05db1dec7
Abdul Hashimi <awm.hashimi@gmail.com>
2022-09-28 12:29:09 -0400

    102

--- src/replit102.java
+++ src/replit102.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class replit102
 {
+    public static void main(String[] args)
+    {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("In:");
+        String word = inp.nextLine();
+        //write your code below
 
+        for (int i = 0; i < word.length(); i++)
+        {
+            System.out.println(word.charAt(i));
+        }
+    }
 }
---
+++ src/replit103.java
@@ -0,0 +1,2 @@
+public class replit103 {
+}
