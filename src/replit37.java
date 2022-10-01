ec3333087ba127727c6e9808c33ce662234954cb
Abdul Hashimi <awm.hashimi@gmail.com>
2022-09-30 21:46:53 -0400

    --

---
+++ src/replit37.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class replit37
+{
+    public static void main(String [] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        String str1 = input.nextLine();
+        String str2 = input.nextLine();
+    }
+}
