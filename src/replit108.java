56f9b91bba68b2a839e1781ceb70b31ebb15c1d6
Abdul Hashimi <awm.hashimi@gmail.com>
2022-10-02 19:57:35 -0400

    108

---
+++ src/replit108.java
@@ -0,0 +1,20 @@
+public class replit108
+{
+    public void m1 ()
+    {
+        System.out.println("m1 method implementation");
+    }
+
+    public void m2()
+    {
+        System.out.println("m2 method implementation");
+    }
+
+    public static void main(String[] args)
+    {
+        replit108 test = new replit108();
+
+        test.m1();
+        test.m2();
+    }
+}
